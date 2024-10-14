20bdb13c3772aba462b0370e7c39511999b02d70
imon <eimon@Imons-MacBook-Air.local>
2024-10-15 00:18:26 +0200

    fnally done with clean and pkl file created

---
+++ src/Visualization/visualize.py
@@ -0,0 +1,8 @@
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+## Load data
+
+df = pd.read_pickle()
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -167,8 +167,31 @@ data_merged.columns = [
 
 # Accelerometer:    12.500HZ
 # Gyroscope:        25.000Hz
+sampling = {
+    'acc_x' : 'mean',
+    'acc_y' : 'mean', 
+    'acc_z' : 'mean', 
+    'gyr_x' : 'mean',
+    'gyr_y' : 'mean', 
+    'gyr_z' : 'mean', 
+    'participant':'last',
+    'label':'last',
+    'category':'last', 
+    'set':'last'
+}
 
+data_merged[:1000].resample(rule="200ms").apply(sampling)
+
+#split by day
+days = [g for n, g in data_merged.groupby(pd.Grouper(freq="D"))]
+
+data_resampled = pd.concat([df.resample(rule="200ms").apply(sampling).dropna() for df in days])
+
+data_resampled.info()
+data_resampled["set"] = data_resampled["set"].astype(int) 
 
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
+
+data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
